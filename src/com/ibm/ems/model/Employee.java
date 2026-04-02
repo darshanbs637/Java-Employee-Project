699d2b7c2166ba8d28696fc75f3bed8918d2df5d
Amal Saji <amal.saji@ibm.com>
2026-04-02 15:09:02 +0530

    latest changes

--- src/com/ibm/ems/model/Employee.java
+++ src/com/ibm/ems/model/Employee.java
@@ -1,27 +1,40 @@
 package com.ibm.ems.model;
 
 public abstract class Employee {
-    private int id;
-    private String name;
-    private double salary;
+	private int id;
+	private String name;
+	private double salary;
 
-    public Employee(int id, String name, double salary) {
-        this.id = id;
-        this.name = name;
-        this.salary = salary;
-    }
+	public Employee(int id, String name, double salary) {
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
 
-    public int getId() { return id; }
-    public String getName() { return name; }
-    public double getSalary() { return salary; }
+	public int getId() {
+		return id;
+	}
 
-    public void setName(String name) { this.name = name; }
-    public void setSalary(double salary) { this.salary = salary; }
+	public String getName() {
+		return name;
+	}
 
-    public abstract double calculateSalary();
+	public double getSalary() {
+		return salary;
+	}
 
-    @Override
-    public String toString() {
-        return id + "," + name + "," + salary + "," + this.getClass().getSimpleName();
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public abstract double calculateSalary();
+
+	@Override
+	public String toString() {
+		return id + "," + name + "," + salary + "," + this.getClass().getSimpleName();
+	}
 }

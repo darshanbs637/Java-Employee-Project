e9039993c721157c2d15cfb8fd6365a5a4939cd6
Rahees VP <rahees.vp@ibm.com>
2026-04-02 15:01:34 +0530

    fixed the salary addition

--- src/com/ibm/ems/service/EmployeeService.java
+++ src/com/ibm/ems/service/EmployeeService.java
@@ -1,7 +1,6 @@
 package com.ibm.ems.service;
-import com.ibm.ems.model.*;
 import com.ibm.ems.exception.EmployeeNotFoundException;
-
+import com.ibm.ems.model.*;
 import java.util.*;
 import java.util.concurrent.*;
 
@@ -73,6 +72,7 @@ public class EmployeeService {
 	            executor.submit(() -> {
 	                double total = e.calculateSalary();
 	                System.out.println(e.getName() + " Final Salary: " + total);
+					e.setSalary(total);
 	            });
 	        }
 
